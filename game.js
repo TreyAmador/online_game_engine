377a84219a019432d780b9cc62df100ddbfccf16
Trey Amador <tmamador@cpp.edu>
2017-03-08 21:34:35 -0800

    canvas interface

--- game.js
+++ game.js
@@ -18,10 +18,47 @@ var KEY = Object.freeze({
 });
 
 
+class Input {
+    constructor() {
+        this.held_keys = new Map();
+        this.pressed_keys = new Map();
+        this.released_keys = new Map();
+    }
+    begin_new_frame() {
+        this.pressed_keys.clear();
+        this.released_keys.clear();
+    }
+    key_down_event(evnt) {
+        this.pressed_keys.set(evnt.keyCode,true);
+        this.held_keys.set(evnt.keyCode,true);
+    }
+    key_up_event(evnt) {
+        this.released_keys.set(evnt.keyCode,true);
+        this.held_keys.set(evnt.keyCode,false);
+    }
+    was_key_pressed(key) {
+        return pressed_keys.has(key);
+    }
+    was_key_released(key) {
+        return released_keys.has(key);
+    }
+    is_key_held(key) {
+        return held_keys.has(key);
+    }
+}
+
+
 class Player {
-    constructor(x,y) {
+    constructor(w,h,x,y,sprite) {
+        this.width = w;
+        this.height = h;
         this.x = x;
         this.y = y;
+        var ctx = GameCore.context;
+        // update method for this
+        // have sprite, not color
+        ctx.fillStyle = sprite;
+        ctx.fillRect(this.x,this.y,this.width,this.height);
     }
     move_up(delta) {
         this.y -= delta;
@@ -51,14 +88,30 @@ class Player {
     }
 }
 
-
-function begin() {
-    return new Player(0,0);
-}
-var player = begin();
+var player;
 
 
 $(document).keydown(function(e){
     player.controller(e);
 });
+
+
+var GameCore = {
+    canvas: document.createElement('canvas'),
+    start: function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext('2d');
+        document.body.insertBefore(
+            this.canvas,
+            document.body.childNodes[0]);
+    }
+}
+
+
+function run() {
+    GameCore.start();
+    player = new Player(30,30,10,120,'red');
+}
+
 
