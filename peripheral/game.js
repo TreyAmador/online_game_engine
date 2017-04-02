351a8731217c97e26bcf20acd3f72ae2ae7874f5
Trey Amador <tmamador@cpp.edu>
2017-04-02 02:23:22 -0700

    engine update

--- peripheral/game.js
+++ peripheral/game.js
@@ -2,6 +2,59 @@
 
 
 
+var Sprite = function(x,y,w,h,bmp) {
+
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.bmp = bmp;
+
+    this.update = function(context) {
+        var ctx = context;
+        ctx.fillStyle = this.bmp;
+        ctx.fillRect(this.x,this.y,this.w,this.h);
+    }
+
+}
+
+
+var Core = function() {
+
+    this.canvas = document.createElement('canvas');
+    
+    this.initialize = function(){   
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext('2d');
+        document.body.insertBefore(
+            this.canvas,document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.player = new Sprite(10,120,30,30,'red');
+        this.interval = setInterval(this.update,20);
+    }
+
+    this.clear = function() {
+        this.context.clearRect(
+            0,0,this.canvas.width,this.canvas.height);
+    }
+
+    this.update = function() {
+        this.player.update(this.context);
+        console.log('hey');
+    }
+
+    
+
+
+}
+
+
+
+function start() {
+    core = new Core();
+    //core.initialize();
+}
 
 
 
@@ -12,8 +65,276 @@
 
 
 
+/*
+
+//var player;
 
 
+
+class Sprite {
+    constructor(x,y,w,h,bmp) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.bmp = bmp;
+    }
+    update(context) {
+        var ctx = context;
+        ctx.fillStyle = this.bmp;
+        ctx.fillRect(this.x,this.y,this.w,this.h);
+    }
+}
+
+
+
+
+class Core {
+    constructor() {
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        document.body.insertBefore(
+            this.canvas,document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(this.update,20);
+        this.context = this.canvas.getContext('2d');
+        this.player = new Sprite(10,120,30,30,'red');
+    }
+    clear() {
+        this.context.clearRect(
+            0,0,this.canvas.width,this.canvas.height);
+    }
+    update() {
+        this.clear();
+        this.player.update(this.context);
+        
+    }
+}
+
+
+
+function start() {
+    core = new Core();
+
+}
+
+*/
+
+
+/*
+
+function Sprite(x,y,w,h,bmp) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.bmp = bmp;
+}
+
+
+Sprite.prototype.update = function(context) {
+    var ctx = context;
+    ctx.fillStyle = this.bmp;
+    ctx.fillRect(this.x,this.y,this.w,this.h);
+}
+
+
+
+
+function Core() {
+    this.canvas = document.createElement('canvas');
+}
+
+
+Core.prototype.initialize = function() {
+    // game loop here
+    // init player, sprite, map, etc
+    
+    this.canvas.width = 480;
+    this.canvas.height = 270;
+    context = this.canvas.getContext('2d');
+    document.body.insertBefore(
+        this.canvas,document.body.childNodes[0]);
+    this.frameNo = 0;
+    this.interval = setInterval(this.update,20);
+
+    //this.player = new Sprite(10,120,30,30,'red');
+    player = new Sprite(10,120,30,30,'red');
+    console.log(player);
+
+}
+
+
+Core.prototype.clear = function() {
+    this.context.clearRect(
+        0,0,this.canvas.width,this.canvas.height);
+}
+
+
+Core.prototype.update = function() {
+    
+    player.update(context);
+
+    //console.log('update');
+    // sprite.update(this);
+    //this.player.update(this.context);
+    //player.update(this.context);
+
+}
+
+
+function start() {
+    core = new Core();
+    //var core = new Core();
+    core.initialize();
+}
+
+*/
+
+
+
+
+
+
+/*
+function Sprite(x,y,w,h,bmp) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.bmp = bmp;
+
+    this.update = function(context) {
+        var ctx = context;
+        ctx.fillStyle = this.bmp;
+        ctx.fillRect(this.x,this.y,this.w,this.h);
+    }
+}
+
+
+
+
+function Core() {
+
+    this.canvas = document.createElement('canvas');
+    this.context = this.canvas.getContext('2d');
+    //this.player;
+    this.player = new Sprite(10,120,30,30,'red');
+
+    this.initialize = function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        //this.context = this.canvas.getContext('2d');
+        document.body.insertBefore(
+            this.canvas,document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.player = new Sprite(10,120,30,30,'red');
+        
+        this.interval = setInterval(this.update,20);
+        //console.log(this.player);
+    }
+
+    this.clear = function() {
+        this.context.clearRect(
+            0,0,this.canvas.width,this.canvas.height);
+    }
+
+    this.update = function() {
+        //console.log(canvas.width)
+        //this.player.update(this.context);
+        console.log(this.prototype.player);
+    }
+
+
+}
+
+
+
+function start() {
+    core = new Core();
+    //var core = new Core();
+    core.initialize();
+}
+
+*/
+
+
+
+
+/*
+
+var core;
+var player;
+
+
+function Sprite(x,y,w,h,bmp) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.bmp = bmp;
+}
+
+
+Sprite.prototype.update = function(context) {
+    var ctx = context;
+    ctx.fillStyle = this.bmp;
+    ctx.fillRect(this.x,this.y,this.w,this.h);
+}
+
+
+
+
+function Core() {
+    this.canvas = document.createElement('canvas');
+}
+
+
+Core.prototype.initialize = function() {
+    // game loop here
+    // init player, sprite, map, etc
+    
+    this.canvas.width = 480;
+    this.canvas.height = 270;
+    this.context = this.canvas.getContext('2d');
+    document.body.insertBefore(
+        this.canvas,document.body.childNodes[0]);
+    this.frameNo = 0;
+    this.interval = setInterval(this.update,20);
+
+    //this.player = new Sprite(10,120,30,30,'red');
+    player = new Sprite(10,120,30,30,'red');
+    console.log(player);
+
+}
+
+
+Core.prototype.clear = function() {
+    this.context.clearRect(
+        0,0,this.canvas.width,this.canvas.height);
+}
+
+
+Core.prototype.update = function() {
+    
+    console.log();
+
+    //console.log('update');
+    // sprite.update(this);
+    //this.player.update(this.context);
+    //player.update(this.context);
+
+}
+
+
+function start() {
+    core = new Core();
+    //var core = new Core();
+    core.initialize();
+}
+
+*/
 
 
 
