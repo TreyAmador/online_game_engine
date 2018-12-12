36a4bcf038b6272c040f132aebaa5607936a5069
Trey Amador <tmamador@cpp.edu>
2018-12-11 19:52:43 -0800

    added media manager

--- scroller/js/game.js
+++ scroller/js/game.js
@@ -8,37 +8,30 @@ var FRAME_PER_SEC = 30;
 var MSEC_PER_SEC = 1000;
 var FRAME_RATE = MSEC_PER_SEC / FRAME_PER_SEC;
 
-var WINDOW_HEIGHT = 720;
-var WINDOW_WIDTH = 1280;
+
+var WINDOW = Object.freeze({
+    WIDTH: 1280,
+    HEIGHT: 720
+});
 
 
+var MediaManager = {
 
+    files: {},
 
-/*
+    load: function(filepath) {
 
-function testDraw() {
+        if (!this.files[filepath]) {
+            var img = new Image();
+            img.src = filepath;
+            this.files[filepath] = img;
+        }
+        
+        return this.files[filepath];
 
-    var sprite = new Image();
-    sprite.src = "img/frame-sprite-animation.png";
-
-    var canvas = document.getElementById('game-canvas');
-    var context = canvas.getContext('2d');
-
-    var i = 0;
-    var frames = 5;
-    var w = 80;
-    var h = 80;
-
-    setInterval(function() {
-        context.drawImage(sprite, 
-            i*w, 0, w, h,
-            0, 0, w, h);
-        i = (++i) % frames;
-    }, 500);
+    }
 
 }
-
-*/
 
 
 function Point2D(x, y) {
@@ -47,18 +40,15 @@ function Point2D(x, y) {
 }
 
 
-function Sprite(path, frames, width, height) {
+function Sprite(filepath, frames, width, height) {
 
     this.frames = frames;
     this.f = -1;
 
-    //this.x = 0;
-    //this.y = 0;
     this.w = width;
     this.h = height;
-    
-    this.image = new Image();
-    this.image.src = path;
+
+    this.image = MediaManager.load(filepath);
 
 }
 
@@ -84,10 +74,8 @@ function Core() {
 
     this.canvas = this.createCanvas('.game-latch');
     this.context = this.canvas.getContext('2d');
-    
+
 }
-
-
 
 
 Core.prototype.createCanvas = function(tag) {
@@ -96,8 +84,8 @@ Core.prototype.createCanvas = function(tag) {
         class: 'game-canvas',
         id: 'game-canvas'
     }).prop({
-        width: WINDOW_WIDTH,
-        height: WINDOW_HEIGHT,
+        width: WINDOW.WIDTH,
+        height: WINDOW.HEIGHT,
     });
 
     $(tag).append(canvas);
@@ -109,18 +97,17 @@ Core.prototype.createCanvas = function(tag) {
 Core.prototype.run = function() {
 
     this.sprite = new Sprite(
-        'img/frame-sprite-animation.png', 
+        'img/cavestorysprite.bmp', 
         [
             new Point2D(0, 0), 
             new Point2D(1, 0), 
+            new Point2D(0, 0), 
             new Point2D(2, 0), 
-            new Point2D(3, 0), 
-            new Point2D(4, 0), 
         ], 
-        80, 80);
+        32, 32);
 
     // TODO remove this rate
-    FRAME_RATE = 1000;
+    FRAME_RATE = 100;
 
     var self = this;
     setInterval(function() {
